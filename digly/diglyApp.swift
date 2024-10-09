e91153dc46065072d8379229b02aabc62997947f
Hyeongseok Kim <hyeongseok@coconutsilo.com>
2024-10-09 18:18:39 +0900

    first commit

---
+++ digly/diglyApp.swift
@@ -0,0 +1,17 @@
+//
+//  diglyApp.swift
+//  digly
+//
+//  Created by 김 형석 on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct diglyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ diglyTests/diglyTests.swift
@@ -0,0 +1,17 @@
+//
+//  diglyTests.swift
+//  diglyTests
+//
+//  Created by 김 형석 on 10/9/24.
+//
+
+import Testing
+@testable import digly
+
+struct diglyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
