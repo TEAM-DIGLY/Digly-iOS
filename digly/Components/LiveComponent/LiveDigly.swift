9db2caff3463fad96999217d442878637f42505d
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-11-19 12:09:55 +0900

    feat: LiveDigly 애니메이션 시점 고도화 (#1)

--- digly/Components/LiveComponent/LiveDigly.swift
+++ digly/Components/LiveComponent/LiveDigly.swift
@@ -7,8 +7,9 @@
 import SwiftUI
 
 struct LiveDigly : View {
-    var isFocused: Bool = false
-    var value: String = ""
+    var isSurprised: Bool = false
+    var readingValue: String = ""
+    var onStareUp: Bool = false
     
     @State private var eyeXOffset: CGFloat = 0
     @State private var eyeYOffset: CGFloat = 0
@@ -32,14 +33,25 @@ struct LiveDigly : View {
             .offset(x: eyeXOffset)
             .offset(y: 6 + eyeYOffset)
         }
-        .onChange(of: isFocused){
-            if !isFocused {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                    withAnimation(.longEaseInOut){
+          
+        .onChange(of: isSurprised){
+            if !isSurprised {
+                // Digly 섹션으로 넘어가면서 TextField focus 해제되는 경우
+                if onStareUp {
+                    withAnimation(.mediumEaseInOut){
                         eyeXOffset = 0
-                        eyeYOffset = 0
-                    } }
+                        eyeYOffset = -12
+                    }
+                } else { // username 변경 세션에서 TextField focus 해제되는 경우
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        withAnimation(.longEaseInOut){
+                            eyeXOffset = 0
+                            eyeYOffset = 0
+                        } }
+                }
             } else {
+                //TODO: FOCUS 상태가 해제되지 않은 상태에서 바로 디글리 선택 세션으로 이동할때, onStareup 애니메이션 적용 안되는 이슈 수정
+                // TextField focus 상태일 경우, 눈 위치 아래로 이동
                 withAnimation(.spring(response:0.2,dampingFraction: 0.5)) { eyeSize = 14 }
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                     withAnimation(.longEaseInOut){
@@ -48,7 +60,9 @@ struct LiveDigly : View {
                     } }
             }
         }
-        .onChange(of: value) {
+        
+        .onChange(of: readingValue) {
+            // TextField 내부 글자 개수에 따라 눈동자 XOffset 실시간 변경
             let progress = CGFloat($1.count) / 15.0
             withAnimation(.spring(response: 0.7, dampingFraction: 0.6)){
                 eyeXOffset = min(10,-10 + (progress * 20))
