25e1bcc5d1477b7dbb7c0dc7167831c1abfa2136
Hyeongseok Kim <neoself1105@gmail.com>
2025-07-30 23:23:33 +0900

    feat: APIERror 추가 (#1)

---
+++ Digly/Sources/Infrastructure/Extensions/Codable+.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+extension Encodable {
+    func toDictionary() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self),
+              let dictionary = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            return [:]
+        }
+        return dictionary
+    }
+}
+
+extension JSONDecoder {
+    static var diglyDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        
+        // Date decoding strategy for ISO8601 format
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+        
+        decoder.dateDecodingStrategy = .custom { decoder in
+            let container = try decoder.singleValueContainer()
+            let dateString = try container.decode(String.self)
+            
+            if let date = ISO8601DateFormatter().date(from: dateString) {
+                return date
+            } else if let date = dateFormatter.date(from: dateString) {
+                return date
+            } else {
+                throw DecodingError.dataCorruptedError(
+                    in: container,
+                    debugDescription: "Cannot decode date string \(dateString)"
+                )
+            }
+        }
+        
+        return decoder
+    }
+}
