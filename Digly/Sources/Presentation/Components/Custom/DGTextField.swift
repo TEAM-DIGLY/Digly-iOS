b1d1db6e673cf0d5c9303c8e8f6cd2cd80340b8c
Hyeongseok Kim <neoself1105@gmail.com>
2025-11-23 21:00:40 +0900

    refactor: DGTextFieldType 적용 (#13)

--- Digly/Sources/Presentation/Components/Custom/DGTextField.swift
+++ Digly/Sources/Presentation/Components/Custom/DGTextField.swift
@@ -1,29 +1,124 @@
 import SwiftUI
 
+enum TextFieldType {
+    case createTicketOptional
+    case createTicket
+    case inquiry
+    case inquiryEmail
+    case profileSetting
+    
+    var textColor: Color {
+        switch self {
+        case .createTicketOptional:
+                .neutral100
+        case .createTicket:
+                .neutral100
+        case .inquiry, .inquiryEmail:
+                .text0
+        case .profileSetting:
+                .text0
+        }
+    }
+    
+    var placeholderColor: Color {
+        switch self {
+        case .createTicketOptional:
+                .opacityWhite300
+        case .inquiry, .inquiryEmail:
+                .opacityWhite300
+        case .createTicket:
+                .opacityWhite300
+        case .profileSetting:
+                .opacityWhite300
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .createTicketOptional:
+                .opacityWhite50
+        case .createTicket:
+                .opacityWhite50
+        case .inquiry, .inquiryEmail:
+                .neutral50
+        case .profileSetting:
+                .opacityWhite800
+        }
+    }
+    
+    var borderColor: Color {
+        switch self {
+        case .createTicketOptional:
+                .opacityWhite100
+        case .createTicket:
+                .opacityWhite100
+        case .inquiry, .inquiryEmail:
+                .clear
+        case .profileSetting:
+                .sbDefault
+        }
+    }
+    
+    var cursorColor: Color {
+        switch self {
+        case .createTicketOptional:
+                .common100
+        case .createTicket:
+                .common100
+        case .inquiry, .inquiryEmail:
+                .text0
+        case .profileSetting:
+                .text0
+        }
+    }
+    
+    var isDeleteButtonPresent: Bool {
+        switch self {
+        case .createTicketOptional:
+            false
+        case .createTicket:
+               true
+        case .inquiry, .inquiryEmail:
+            true
+        case .profileSetting:
+            true
+        }
+    }
+    
+    var keyboardType: UIKeyboardType {
+        switch self {
+        case .createTicketOptional:
+                .default
+        case .createTicket:
+                .default
+        case .inquiry:
+                .default
+        case .inquiryEmail:
+                .emailAddress
+        case .profileSetting:
+                .default
+        }
+    }
+}
+
+
 struct DGTextField: View {
     @Binding var text: String
     @FocusState private var isFocused: Bool
-    
     let placeholder: String
-    var keyboardType: UIKeyboardType = .default
-    var textColor: Color = .text0
-    var placeholderColor: Color = .neutral300
-    var backgroundColor: Color = .opacityWhite800
-    var borderColor: Color = .sbDefault
-    var cursorColor: Color = .text0
+    let type: TextFieldType
     
-    var isDeleteButtonPresent: Bool = false
     var onSubmit: (() -> Void)?
     
     var body: some View {
         ZStack(alignment: .trailing) {
             TextField("", text: $text, prompt: prompt)
-                .keyboardType(keyboardType)
+                .keyboardType(type.keyboardType)
                 .focused($isFocused)
             
-                .tint(cursorColor)
+                .tint(type.cursorColor)
                 .fontStyle(.headline1)
-                .foregroundStyle(textColor)
+                .foregroundStyle(type.textColor)
                 .frame(maxWidth: .infinity)
             
             Button(action: {
@@ -31,19 +126,18 @@ struct DGTextField: View {
             }) {
                 Image("x_circle")
                     .renderingMode(.template)
-                    .foregroundStyle(cursorColor)
+                    .foregroundStyle(type.cursorColor)
                     .padding(.horizontal, 8)
             }
-            .background(backgroundColor.opacity(0.5))
-            .opacity(isFocused ? 1 : 0)
-            .scaleEffect(isFocused ? 1 : 0)
+            .opacity(type.isDeleteButtonPresent && isFocused ? 1 : 0)
+            .scaleEffect(type.isDeleteButtonPresent && isFocused ? 1 : 0)
         }
         .autocorrectionDisabled()
         .autocapitalization(.none)
         
         .padding(.leading, 16)
         .frame(height: 56, alignment: .center)
-        .background(backgroundColor, in: RoundedRectangle(cornerRadius: 16))
+        .background(type.backgroundColor, in: RoundedRectangle(cornerRadius: 16))
         .animation(.mediumSpring, value: isFocused)
         .onSubmit { onSubmit?() }
     }
@@ -51,6 +145,6 @@ struct DGTextField: View {
     private var prompt: Text {
         Text(placeholder)
             .font(.headline1)
-            .foregroundColor(placeholderColor)
+            .foregroundColor(type.placeholderColor)
     }
 }
